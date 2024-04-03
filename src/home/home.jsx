95ac7a8ad4f13c7fb3b10d4011caa668b629423f
Kyle Tennison <kyletennison05@gmail.com>
2024-04-03 12:15:06 -0700

    add cad structure

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,14 +1,17 @@
-import "./home.css"
+import ProjectCard from "./project-card"
 
-import linkedin_icon from "../assets/linkedin.svg"
-import twitter_icon from "../assets/twitter-x.svg"
-import github_icon from "../assets/github.svg"
-import background_image from "../assets/header.jpg"
+import "./home.css"
+import linkedin_icon from "../assets/icons/linkedin.svg"
+import twitter_icon from "../assets/icons/twitter-x.svg"
+import github_icon from "../assets/icons/github.svg"
+import background_image from "../assets/icons/header.jpg"
 import selfie_image from "../assets/selfie.jpg"
-import cake_icon from "../assets/cake2.svg"
-import mortarboard_icon from "../assets/mortarboard.svg"
-import book_icon from "../assets/book.svg"
-import location_icon from "../assets/geo-alt.svg"
+import cake_icon from "../assets/icons/cake2.svg"
+import mortarboard_icon from "../assets/icons/mortarboard.svg"
+import book_icon from "../assets/icons/book.svg"
+import location_icon from "../assets/icons/geo-alt.svg"
+
+import magnetite_img from "../assets/project-pictures/magnetite.png"
 
 export default function Home(){
 
@@ -19,10 +22,10 @@ export default function Home(){
         <h1>Kyle Tennison</h1>
         <h2>Engineering Student and Software Developer</h2>
         <div id="social-icons">
-
-            <img className="icon" src={twitter_icon}></img> 
-            <img className="icon" src={github_icon} onClick={()=> window.open("https://github.com/kyle-tennison", "_blank")}></img>
-            <img className="icon" src={linkedin_icon} onClick={()=> window.open("https://www.linkedin.com/in/kyle-tennison-1704631b0/", "_blank")}></img>
+``
+            <img className="icon clickable" src={twitter_icon}></img> 
+            <img className="icon clickable" src={github_icon} onClick={()=> window.open("https://github.com/kyle-tennison", "_blank")}></img>
+            <img className="icon clickable" src={linkedin_icon} onClick={()=> window.open("https://www.linkedin.com/in/kyle-tennison-1704631b0/", "_blank")}></img>
 
         </div>
         <p>kyletennison05@gmail.com</p>
@@ -72,6 +75,27 @@ export default function Home(){
     <div id="project-list">
         <h1>What I&apos;ve Been Up To</h1>
         <a>The following are some projects I&apos;ve been working on. Feel free to shoot me a question about any of them!</a>
+
+        <div className="flex-container cards-container">
+            <ProjectCard 
+            img={magnetite_img} 
+            title="Magnetite FEA"
+            text="A 2D Finite-Element Method Mechanical Simulator, built in Rust."
+            icon={github_icon}
+            icon_text="Check it out on GitHub"
+            link="https://github.com/kyle-tennison/Magnetite"
+            date="March 2024"
+            />
+            <ProjectCard 
+            img={magnetite_img} 
+            title="Magnetite FEA"
+            text="A 2D Finite-Element Method Mechanical Simulator, built in Rust."
+            icon={github_icon}
+            icon_text="Check it out on GitHub"
+            link="https://github.com/kyle-tennison/Magnetite"
+            date="March 2024"
+            />
+        </div>
 
     </div>
 
---
+++ src/home/project-card.jsx
@@ -0,0 +1,28 @@
+export default function ProjectCard(props){
+
+    return <div className="project-card clickable" onClick={()=>{window.open(props.link, "_blank")}}>
+
+        <div className="flex-container">
+
+            <div className="left">
+                <img src={props.img} />
+            </div>
+
+            <div className="right">
+
+                <h2>{props.title}</h2>
+                <a>{props.text}</a>
+                
+                <div className="icon-text-pair">
+                    <img src={props.icon}/>
+                    <a>{props.icon_text}</a>
+                </div>
+
+
+                <a id="date">{props.date}</a>
+            </div>
+        </div>
+
+    </div>
+
+}
