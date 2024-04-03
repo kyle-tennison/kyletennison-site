1e03d2a0cd532a02d7e6cb694236e35d7eb86aef
Kyle Tennison <135751865+kyle-tennison@users.noreply.github.com>
2024-04-03 13:19:50 -0700

    Home page (#1)
    * about me section
    
    * clickable header links
    
    * add cad structure
    
    * add card content
    
    * update readme.md
    
    * add icon
    
    * add screenshot to readme

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# [kyletennison.com](https://kyletennison.com)
 
-Currently, two official plugins are available:
+![A screenshot of the website](screenshot.png)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The source code for my portfolio website.
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,21 @@
-import "./home.css"
+import ProjectCard from "./project-card"
 
-import linkedin_icon from "../assets/linkedin.svg"
-import twitter_icon from "../assets/twitter-x.svg"
-import github_icon from "../assets/github.svg"
-import background_image from "../assets/header.jpg"
+import "./home.css"
+import linkedin_icon from "../assets/icons/linkedin.svg"
+import twitter_icon from "../assets/icons/twitter-x.svg"
+import github_icon from "../assets/icons/github.svg"
+import background_image from "../assets/icons/header.jpg"
+import selfie_image from "../assets/selfie.jpg"
+import cake_icon from "../assets/icons/cake2.svg"
+import mortarboard_icon from "../assets/icons/mortarboard.svg"
+import book_icon from "../assets/icons/book.svg"
+import location_icon from "../assets/icons/geo-alt.svg"
+import boxes_icon from "../assets/icons/boxes.svg"
+
+import magnetite_img from "../assets/project-pictures/magnetite.png"
+import pyrite_img from "../assets/project-pictures/pyrite.png"
+import climber_img from "../assets/project-pictures/climber.jpg"
+import franklin_img from "../assets/project-pictures/franklin.png"
 
 export default function Home(){
 
@@ -14,15 +26,100 @@ export default function Home(){
         <h1>Kyle Tennison</h1>
         <h2>Engineering Student and Software Developer</h2>
         <div id="social-icons">
-
-            <img src={twitter_icon}></img>
-            <img src={github_icon}></img>
-            <img src={linkedin_icon}></img>
+``
+            <img className="icon clickable" src={twitter_icon}></img> 
+            <img className="icon clickable" src={github_icon} onClick={()=> window.open("https://github.com/kyle-tennison", "_blank")}></img>
+            <img className="icon clickable" src={linkedin_icon} onClick={()=> window.open("https://www.linkedin.com/in/kyle-tennison-1704631b0/", "_blank")}></img>
 
         </div>
         <p>kyletennison05@gmail.com</p>
     </div>
+    <div id="about-me">
+        <h1>About Me</h1>
 
+        <div className="flex-container">
+            <div className="left" id="selfie">
+                <img src={selfie_image}/>
+            </div>
+            <div className="right">
+                <a>
+                Hi! I&apos;m Kyle — an engineering student from California. I&apos;m majoring in 
+                Mechanical Engineering, but I spend a lot of my energy with software engineering; 
+                I&apos;m trying to get well rounded in both subjects. This page is dedicated to 
+                showcasing some of the projects I&apos;ve been working on. I&apos;m a hands-on person, 
+                so this is my way of learning.
+                </a>
+                <div className="flex-container">
+                    <div className="info-card left">
+                        <div className="icon-text-pair">
+                            <img className="icon" src={mortarboard_icon}/>
+                            <a style={{textTransform: "underline"}} href="https://canadacollege.edu">Cañada College</a>
+                        </div>
+                        <div className="icon-text-pair">
+                            <img className="icon" src={book_icon}/>
+                            <a style={{textTransform: "underline"}}>Studying Mechanical Engineering</a>
+                        </div>
+                    </div>
+                    <div className="info-card right">
+                        <div className="icon-text-pair">
+                            <img className="icon" src={cake_icon}/>
+                            <a>February 1st, 2005</a>
+                        </div>
+
+                        <div className="icon-text-pair">
+                            <img className="icon" src={location_icon}/>
+                            <a>Bay Area, California</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="project-list">
+        <h1>What I&apos;ve Been Up To</h1>
+        <a>The following are some projects I&apos;ve been working on. Feel free to shoot me a question about any of them!</a>
+
+        <div className="flex-container cards-container">
+            <ProjectCard 
+            img={magnetite_img} 
+            title="Magnetite FEA"
+            text="A 2D Finite-Element Method Mechanical Simulator, built in Rust."
+            icon={github_icon}
+            icon_text="Check it out on GitHub"
+            link="https://github.com/kyle-tennison/Magnetite"
+            date="March-April 2024"
+            />
+            <ProjectCard 
+            img={pyrite_img} 
+            title="Pyrite FEA"
+            text="Another 2D FEA Simulator built Natively in Python."
+            icon={github_icon}
+            icon_text="Check it out on GitHub"
+            link="https://github.com/kyle-tennison/Pyrite"
+            date="March 2024"
+            />
+            <ProjectCard 
+            img={franklin_img} 
+            title="Franklin"
+            text="A self-balancing robot friend. Work in progress."
+            icon={github_icon}
+            icon_text="Check it out on GitHub"
+            link="https://github.com/kyle-tennison/franklin"
+            date="December-Now 2023"
+            />
+            <ProjectCard 
+            img={climber_img} 
+            title="5940 BREAD 2023 Climber"
+            text="Climber Subsystem for the FRC 2023 Competition."
+            icon={boxes_icon}
+            icon_text="Check out the Model"
+            link="https://cad.onshape.com/documents/f24fe5dbb9a6ebea2bf3693f"
+            date="January-March 2023"
+            />
+        </div>
+
+    </div>
 
     </div>
 
